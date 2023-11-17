b5b4ecb7072a1ae8bdacda7894caedf80cd552e2
yashwanthwebdev <yashwanthsrinivaswebdev@gmail.com>
2023-11-17 12:59:35 +0530

    alignment fixes. font changes

--- src/App.js
+++ src/App.js
@@ -1,68 +1,123 @@
-import 'antd/dist/antd.min.css'
-import Login from './components/Login/Login'
-import {useState, useEffect, useRef} from 'react'
-import ExtractTodoList from './components/Firebase-utils/ExtractTodoList';
-import AddPost from './components/Firebase-utils/AddPost/AddPost' 
-import style from './App.module.css'
-import { updateDoc, doc, deleteDoc } from 'firebase/firestore';
-import {db} from './components/Firebase-utils/Firebase-config'
-import {collection} from 'firebase/firestore'
- import {getDocs, query, where} from 'firebase/firestore'
-
+import "antd/dist/antd.min.css";
+import Login from "./components/Login/Login";
+import { useState, useEffect, useRef } from "react";
+import ExtractTodoList from "./components/Firebase-utils/ExtractTodoList";
+import AddPost from "./components/Firebase-utils/AddPost/AddPost";
+import style from "./App.module.css";
+import { updateDoc, doc, deleteDoc } from "firebase/firestore";
+import { db } from "./components/Firebase-utils/Firebase-config";
+import { collection } from "firebase/firestore";
+import { getDocs, query, where } from "firebase/firestore";
+import { Typography } from "@mui/material";
+import Stack from "@mui/material/Stack";
 
 function App() {
-  const [user,setUser] = useState(null)
+  const [user, setUser] = useState(null);
   const [todoList, setTodoList] = useState(null);
-  const [editable,setEditable] = useState("null")
+  const [editable, setEditable] = useState("null");
   const activityRef = useRef("");
   const inputRef = useRef();
- 
 
-  useEffect ( ()=> {
+  useEffect(() => {
     console.log("updated todo list is , ", todoList);
-    }, [todoList])
+  }, [todoList]);
 
-   useEffect ( ()=> {
+  useEffect(() => {
     console.log("editable toggled to ", editable);
-   
-   },[editable])
+  }, [editable]);
 
-  
-const deleteDocId = async (id) => {
-  console.log("going to delete doc with id ", id)
-  const docRef = await doc(db, "todo", id);
-    deleteDoc(docRef).then(()=>{console.log("deleted doc")})
- 
-var tempList= [...todoList].filter( (element)=> element.id != id )
-console.log(tempList);  
-setTodoList( tempList)
-}
+  const deleteDocId = async (id) => {
+    console.log("going to delete doc with id ", id);
+    const docRef = await doc(db, "todo", id);
+    deleteDoc(docRef).then(() => {
+      console.log("deleted doc");
+    });
 
-
+    var tempList = [...todoList].filter((element) => element.id != id);
+    console.log(tempList);
+    setTodoList(tempList);
+  };
 
   return (
-    <div className="App">
-      <div className={style.title} > ToDo app</div>
-      <div className={style.welcomeMessage}>{  user ? 
-      <span style={{color:"rgb(231, 14, 14)",fontSize:"1rem",
-      fontWeight:"800"}}>Welcome, {user.email}</span> : <></>}</div>
-      <Login user={user} setUser={setUser}  />
-      
-       { user!==null   ?
-<ExtractTodoList user={user}  setTodoList={setTodoList} /> 
-: <span></span> }
-     
-{user!==null ? 
-  <div className={style.mainContainer}>  
+    <Stack className="App" direction="column">
+      <Stack
+        direction="column"
+        style={{
+          backgroundColor: "black",
+          height: "8ch",
+          placeItems: "center",
+          placeContent: "center",
+        }}
+      >
+        <Stack direction="row" spacing={2}>
+          <Typography
+            variant="caption"
+            style={{
+              width: "100%",
+              textAlign: "center",
+              color: "white",
+              fontFamily: "cursive",
+              fontSize: "2rem",
+              // fontFamily: "sans-serif",
+              fontStyle: "italic",
+              fontWeight: "800",
+              textDecoration: "underline",
+              textDecorationColor: "aqua",
+            }}
+          >
+            Todo
+          </Typography>
 
-<div >
-  {/* list todo container */}
-{todoList?.length>0 ?  
-    
+          <Typography
+            variant="caption"
+            style={{
+              width: "100%",
+              textAlign: "center",
+              color: "white",
+              fontFamily: "cursive",
+              fontSize: "2rem",
+              // fontFamily: "sans-serif",
+              fontStyle: "italic",
+              fontWeight: "800",
+              backgroundColor: "brown",
+              paddingLeft: "0.2ch",
+              paddingRight: "0.2ch",
+            }}
+          >
+            app
+          </Typography>
+        </Stack>
+      </Stack>
+      <div className={style.welcomeMessage}>
+        {user ? (
+          <span
+            style={{
+              color: "rgb(231, 14, 14)",
+              fontSize: "1rem",
+              fontWeight: "800",
+            }}
+          >
+            Welcome, {user.email}
+          </span>
+        ) : (
+          <></>
+        )}
+      </div>
+      <Login user={user} setUser={setUser} />
 
-    <div className={style.todoList}>
-    
-    {/* <div>
+      {user !== null ? (
+        <ExtractTodoList user={user} setTodoList={setTodoList} />
+      ) : (
+        <span></span>
+      )}
+
+      {user !== null ? (
+        <div className={style.mainContainer}>
+          <div>
+            {/* list todo container */}
+            {todoList?.length > 0 ? (
+              <div className={style.todoList}>
+                {/* <div>
        button  sort by created   
     <button onClick={()=>
             {  var x = []    
@@ -88,74 +143,102 @@ setTodoList( tempList)
             }
             }>Sort (by deadline date)</button>
     </div> */}
-    
-     {/* each todo list container */}
-        {todoList?.map( (eachTodo,index)=> {
-          return <div className={style.eachTodoContainer} 
-          key={eachTodo.id}>
-              <input 
-              ref= {inputRef}
-              defaultValue={eachTodo.activity}   
-              readOnly={editable!==eachTodo.id}
-            className={style.inputActivity}
-             onChange={(event)=>{
-                activityRef.current = event.target.value 
-             }}/>
-             {/* <span>{eachTodo.id}</span>  */}
-           
-            <span>created on  {(new Date(eachTodo.created)).toLocaleString()}</span> 
-            <span>deadline is {eachTodo.deadline}</span> 
-          <div> 
-    
-    {/* button edit */}
-          <button className={style.editButton} onClick={(event)=> {
-              setEditable(eachTodo.id);
-              inputRef.current.focus();
-                }
-          }  
-              >edit</button>
-    
-    {/* button delete */}
-    <button className={style.editButton} onClick={()=>
-       deleteDocId(eachTodo.id)
-          }  
-              >delete</button>
-    
 
-    {/* button done */}
-         {  editable === eachTodo.id ? 
-         <button className={style.doneButton} onClick={(event)=> {setEditable(false);
-          setEditable("null");
-          console.log("input value is ", activityRef.current);
-           const docRef = doc(db, "todo", eachTodo.id);
-           if (activityRef.current !== "" ) {
-            updateDoc(docRef, {
-              activity:  activityRef.current,
-             }).then( ()=> {console.log("updated doc"); activityRef.current=""}) 
-         }
-           
-     }}>Done</button> : <></>   }  
-    </div>
-           </div> // each todo container div
-        } )  }
+                {/* each todo list container */}
+                {todoList?.map((eachTodo, index) => {
+                  return (
+                    <div className={style.eachTodoContainer} key={eachTodo.id}>
+                      <input
+                        ref={inputRef}
+                        defaultValue={eachTodo.activity}
+                        readOnly={editable !== eachTodo.id}
+                        className={style.inputActivity}
+                        onChange={(event) => {
+                          activityRef.current = event.target.value;
+                        }}
+                      />
+                      {/* <span>{eachTodo.id}</span>  */}
 
+                      <span>
+                        created on {new Date(eachTodo.created).toLocaleString()}
+                      </span>
+                      <span>deadline is {eachTodo.deadline}</span>
+                      <div>
+                        {/* button edit */}
+                        <button
+                          className={style.editButton}
+                          onClick={(event) => {
+                            setEditable(eachTodo.id);
+                            inputRef.current.focus();
+                          }}
+                        >
+                          edit
+                        </button>
+
+                        {/* button delete */}
+                        <button
+                          className={style.editButton}
+                          onClick={() => deleteDocId(eachTodo.id)}
+                        >
+                          delete
+                        </button>
+
+                        {/* button done */}
+                        {editable === eachTodo.id ? (
+                          <button
+                            className={style.doneButton}
+                            onClick={(event) => {
+                              setEditable(false);
+                              setEditable("null");
+                              console.log(
+                                "input value is ",
+                                activityRef.current
+                              );
+                              const docRef = doc(db, "todo", eachTodo.id);
+                              if (activityRef.current !== "") {
+                                updateDoc(docRef, {
+                                  activity: activityRef.current,
+                                }).then(() => {
+                                  console.log("updated doc");
+                                  activityRef.current = "";
+                                });
+                              }
+                            }}
+                          >
+                            Done
+                          </button>
+                        ) : (
+                          <></>
+                        )}
+                      </div>
+                    </div>
+                  ); // each todo container div
+                })}
+              </div>
+            ) : (
+              <div style={{ textAlign: "center", color: "white" }}>
+                No todos have been loaded yet.../you haven't added any todos yet
+              </div>
+            )}
+          </div>
+
+          {/* add todo container  */}
+          <div>
+            {user !== null ? (
+              <AddPost
+                user={user}
+                todoList={todoList}
+                setTodoList={setTodoList}
+              />
+            ) : (
+              <></>
+            )}
+          </div>
         </div>
-      : 
-      <div style={{textAlign:"center", color:"white"}}>No todos have been loaded yet.../you haven't added any todos yet</div>}
-
-   </div>
-
-{/* add todo container  */}
- <div>
-{   user!==null   ? 
-    <AddPost user={user} todoList={todoList} setTodoList={setTodoList} />  
-    : <></>}
- </div>
-</div>
-:
-<></>
-}
-    </div>
+      ) : (
+        <></>
+      )}
+    </Stack>
   );
 }
 
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,128 +1,213 @@
-import React from 'react'
-import style from './Login.module.css'
-import {useState, useEffect} from 'react'
-import { ConsoleSqlOutlined } from '@ant-design/icons';
-import {createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut} from 'firebase/auth'
-import {auth} from '../Firebase-utils/Firebase-config'
+import React from "react";
+import style from "./Login.module.css";
+import { useState, useEffect } from "react";
+import Divider from "@mui/material/Divider";
+import { ConsoleSqlOutlined } from "@ant-design/icons";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { auth } from "../Firebase-utils/Firebase-config";
+import Stack from "@mui/material/Stack";
+import { Typography } from "antd";
 // import ToDoList from '../ToDoList';
 
-
 function Login(props) {
-
-  const [ loginEmailId, setLoginEmailId] = useState("");
+  const [loginEmailId, setLoginEmailId] = useState("");
   const [loginPassword, setLoginPassword] = useState("");
   const [registerEmailId, setRegisterEmailId] = useState("");
   const [registerPassword, setRegisterPassword] = useState("");
   const [registrationResult, setRegistrationResult] = useState(false);
-  const [loginError, setLoginError] = useState("")
-//  const [user,setUser] = useState(null)
+  const [loginError, setLoginError] = useState("");
+  //  const [user,setUser] = useState(null)
 
   const registerUser = async () => {
-    console.log("registerd credentials are  ", registerEmailId, " ", registerPassword);
+    console.log(
+      "registerd credentials are  ",
+      registerEmailId,
+      " ",
+      registerPassword
+    );
     try {
-      const tempUser = await createUserWithEmailAndPassword(auth, registerEmailId,registerPassword)
+      const tempUser = await createUserWithEmailAndPassword(
+        auth,
+        registerEmailId,
+        registerPassword
+      );
       console.log("registration successfully done. user is ", tempUser);
       setRegistrationResult(true);
+    } catch (error) {
+      console.log("error is ", error);
     }
-    catch (error){
-      console.log("error is ", error)
-    }
-  }
+  };
 
   const loginUser = async () => {
-    console.log("login credentials are ", loginEmailId, " " , loginPassword)
-   try {
-    const result = await signInWithEmailAndPassword(auth, loginEmailId, loginPassword)
+    console.log("login credentials are ", loginEmailId, " ", loginPassword);
+    try {
+      const result = await signInWithEmailAndPassword(
+        auth,
+        loginEmailId,
+        loginPassword
+      );
       console.log("login is successfull! ", result);
-     
-  
-   }
-   catch (error) {
-     console.log("Wrong username/password ");
-     setLoginError("Wrong username/password")
-   }
-  }
+    } catch (error) {
+      console.log("Wrong username/password ");
+      setLoginError("Wrong username/password");
+    }
+  };
 
-
-  useEffect ( ()=> {
-    onAuthStateChanged( auth, (currentUser) => {
-       props.setUser(currentUser);
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      props.setUser(currentUser);
       console.log("auth state changed to", currentUser);
-        setLoginError("")
-     })
-  },[])
+      setLoginError("");
+    });
+  }, []);
 
   const logout = async () => {
     signOut(auth);
-  }
+  };
 
   return (
     <div className={style.mainContainer}>
-      
-       
-      { props.user ? <div>  
-
-        <button className={style.logoutButton} onClick={(event)=> {
-      logout(event);
-     }}>Logout</button>
-
-  
-      </div> : <>
-   
-      <div>
-        <div> 
-            <span>email id</span>
-            <input type="text" onChange={ (event) => {
-              setLoginEmailId(event.target.value)
-            }}/>
+      {props.user ? (
+        <div>
+          <button
+            className={style.logoutButton}
+            onClick={(event) => {
+              logout(event);
+            }}
+          >
+            Logout
+          </button>
         </div>
+      ) : (
+        <>
+          <Stack direction="column" spacing={1}>
+            <Typography
+              style={{
+                color: "orange",
+                fontSize: "1rem",
+                fontFamily: "monospace",
+              }}
+            >
+              If you already have an account
+            </Typography>
+            <Stack
+              direction="row"
+              style={{
+                width: "40ch",
+                justifyContent: "space-between",
+              }}
+            >
+              <span>email id</span>
+              <input
+                type="text"
+                onChange={(event) => {
+                  setLoginEmailId(event.target.value);
+                }}
+              />
+            </Stack>
 
+            <Stack
+              direction="row"
+              style={{
+                width: "40ch",
+                justifyContent: "space-between",
+              }}
+            >
+              <span>password</span>
+              <input
+                type="text"
+                onChange={(event) => {
+                  setLoginPassword(event.target.value);
+                }}
+              />
+            </Stack>
+            <button
+              style={{ cursor: "pointer" }}
+              onClick={(event) => {
+                loginUser();
+              }}
+            >
+              login
+            </button>
 
-        <div> 
-            <span>password </span>
-            <input type="text"   onChange={ (event) => {
-              setLoginPassword(event.target.value)
-            }}/>
-        </div>
-        <button onClick={(event) => {loginUser()}}>login</button>
+            <span className={style.loginError}>{loginError}</span>
+          </Stack>
 
+          {/* <span> or </span> */}
+          <Divider
+            sx={{
+              borderRadius: "1ch",
+              color: "white",
+              border: "0.1px solid white",
+              width: "30%",
+            }}
+          ></Divider>
 
-        <span className={style.loginError}>{loginError}</span>
-            </div>
+          <Stack direction="column" spacing={1}>
+            <Typography
+              style={{
+                color: "orange",
+                fontSize: "1rem",
+                fontFamily: "monospace",
+              }}
+            >
+              If you don't have an account here yet
+            </Typography>
+            <Stack
+              direction="row"
+              style={{
+                width: "40ch",
+                justifyContent: "space-between",
+              }}
+            >
+              <span>email id</span>
+              <input
+                type="text"
+                onChange={(event) => {
+                  setRegisterEmailId(event.target.value);
+                }}
+              />
+            </Stack>
 
-        <span> or </span>
+            <Stack
+              direction="row"
+              style={{
+                width: "40ch",
+                justifyContent: "space-between",
+              }}
+            >
+              <span>password </span>
+              <input
+                type="text"
+                onChange={(event) => {
+                  setRegisterPassword(event.target.value);
+                }}
+              />
+            </Stack>
 
-
-            <div>
-        <div> 
-            <span>email id</span>
-            <input type="text"   onChange={ (event) => {
-              setRegisterEmailId(event.target.value)
-            }}/>
-        </div>
-
-
-        <div> 
-            <span>password </span>
-            <input type="text"   onChange={ (event) => {
-              setRegisterPassword(event.target.value)
-            }}/>
-        </div>
- 
-        <button onClick={(event) => {registerUser()}}>register</button>
-        {registrationResult==true? <span>registration successfull!</span>: <></>}
-       
- 
-        </div>
-    
-      </>}
-
-
-     
-
-
+            <button
+              style={{ cursor: "pointer" }}
+              onClick={(event) => {
+                registerUser();
+              }}
+            >
+              register
+            </button>
+            {registrationResult == true ? (
+              <span>registration successfull!</span>
+            ) : (
+              <></>
+            )}
+          </Stack>
+        </>
+      )}
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
